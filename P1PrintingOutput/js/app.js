cfed9bac8fb474e88e75814875464ebbea07eef4
Moa-05 <moaber654@ihmeducation.se>
2024-09-12 10:46:16 +0200

    Uppdate

--- P1PrintingOutput/js/app.js
+++ P1PrintingOutput/js/app.js
@@ -1,2 +1,16 @@
 console.log("Hello World!");
 console.log("My name is Moa");
+
+
+const x = 6;
+const y = 3;
+const five = 5;
+const eight = 8;
+let total = 0;
+
+console.log(five);
+console.log(eight);
+
+total = five + eight;
+console.log(five + eight);
+
