74437c57f1326038d86eb795ca41a07555e7f87b
Moa-05 <moaber654@ihmeducation.se>
2024-09-16 10:24:54 +0200

    Refactor

--- P5Operator/js/app.js
+++ P5Operator/js/app.js
@@ -1,48 +1,68 @@
-let x= 5 + 5; // x = 10
-let y= x + 5; // y= 15
-let z = y + y; // z = 30
-console.log(z + z);
+let speedInput = 72;
+let speedOutput = 0;
 
-let firstNumber = 7;
-let secondNumber = 2;
-let answer = 0;
-// 1 + 2 = 3
-answer = firstNumber + secondNumber;
-console.log(firstNumber + secondNumber);
+speedOutput = speedInput / 3.6;
+console.log(speedOutput);
 
-answer = firstNumber - secondNumber;
+let minutesInput = 3;
+let secondOutput = 0;
+
+secondOutput=minutesInput * 60;
+console.log(secondOutput);
+
+let numberInput1 = 11;
+let numberInpiut2 = 4;
+let numberOutput= 0;
+numberOutput3 = numberInput1 / numberInpiut2;
+console.log(numberOutput3);
+
+Display= numberInput1 % numberInpiut2;
+console.log(Display);
+
+
+let radius = 2
+let pi = 3.14
+area = radius * radius * pi;
+console.log(area);
+
+let input1 = 11;
+let inpu2 = -1;
+answer = input1 * (inpu2);
 console.log(answer);
 
-answer = firstNumber * secondNumber;
-console.log(answer);
+let weightinput = 70
+let height = 1.82
+let BMI = weightinput /Math.pow(height, 2);
+console.log(BMI);
 
-answer = firstNumber / secondNumber;
-console.log(answer);
+let sideA = 3;
+let sideB = 4;
+let hypotenuse;
+let squareOfAAandB= Math.pow( sideA, 2)+ Math.pow( sideB, 2);
+hypotenuse = Math.sqrt(squareOfAAandB);
+console.log(hypotenuse);
 
-answer = firstNumber % secondNumber;
-console.log(answer);
+/*
+let secound1 = 111;
+let minuetinput = 60;
+Answer = secound1 / minuetinput;
+console.log(Answer, secound1 % minuetinput);
+*/
 
-let gold = 50;
-gold + 10;
-console.log(gold);
+let secondeinput3 = 111;
+let miuetinput2;
+miuetinput2 = Math.floor(secondeinput3 / 60);
+console.log(miuetinput2);
 
-gold = gold + 10;
-console.log(gold); // 60
+let secondOutputi;
+secondOutputi = secondeinput3 - miuetinput2 * 60;
+secondeinput3 = secondeinput3 % 60;
+miuetinput2 = miuetinput2 + "second" + "minuets";
+console.log(secondOutputi);
 
-gold += 10;
-console.log(gold); // 70
 
-gold *= 10;
-console.log(gold); // 700
 
-gold /= 10;
-console.log(gold); // 70
 
-gold -= 10;
-console.log(gold);
-
-gold %= 2;
-console.log(gold); // 0
 
 
 
