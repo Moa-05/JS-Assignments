e7f6f4fea04f3712ffcc4f24bc936938ff61e02f
Moa-05 <moaber654@ihmeducation.se>
2024-09-16 10:25:15 +0200

    Refactor

---
+++ P5Operator/js/app2.js
@@ -0,0 +1,73 @@
+let x= 5 + 5; // x = 10
+let y= x + 5; // y= 15
+let z = y + y; // z = 30
+console.log(z + z);
+
+let firstNumber = 7;
+let secondNumber = 2;
+let answer = 0;
+// 1 + 2 = 3
+answer = firstNumber + secondNumber;
+console.log(firstNumber + secondNumber);
+
+answer = firstNumber - secondNumber;
+console.log(answer);
+
+answer = firstNumber * secondNumber;
+console.log(answer);
+
+answer = firstNumber / secondNumber;
+console.log(answer);
+
+answer = firstNumber % secondNumber;
+console.log(answer);
+
+let gold = 50;
+gold + 10;
+console.log(gold);
+
+gold = gold + 10;
+console.log(gold); // 60
+
+gold += 10;
+console.log(gold); // 70
+
+gold *= 10;
+console.log(gold); // 700
+
+gold /= 10;
+console.log(gold); // 70
+
+gold -= 10;
+console.log(gold);
+
+gold %= 2;
+console.log(gold); // 0
+
+let numberOfStudents = 0
+
+//numbersOfStudents+= 1;
+//console.log(numberOfStudents);
+
+
+++numberOfStudents;
+console.log(numberOfStudents);
+
+//numberOfStudents--;
+//console.log(numberOfStudents);
+
+console.log(++numberOfStudents);
+
+console.log(--numberOfStudents);
+
+numberOfStudents++;
+console.log(numberOfStudents); // 2
+
+numberOfStudents++;
+console.log(numberOfStudents); // 3
+
+// PRE OR POST
+
+
+
+
