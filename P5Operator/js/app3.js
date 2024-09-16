7787880acecaf7688bbfea0c14ead2badd320249
Moa-05 <moaber654@ihmeducation.se>
2024-09-16 10:43:55 +0200

    refactory

--- P5Operator/js/app3.js
+++ P5Operator/js/app3.js
@@ -1,5 +1,8 @@
+let numberOfBottles = 10
+const line1 = "green bottles hannging on the wall"
+const line2 = " and if one of the green bottles accidently fall there will be"
 
-
-
+console.log(numberOfBottles + line1 + line1)
+console.log(+ --numberOfBottles + line2)
 
 
