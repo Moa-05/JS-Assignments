35a8119e046af45ecce4e75e7662f1158527dd4b
Moa-05 <moaber654@ihmeducation.se>
2024-09-17 09:43:09 +0200

    P10 sum

--- P9-functions/js/app.js
+++ P9-functions/js/app.js
@@ -31,3 +31,53 @@ turnLeft()
 stepForward()
 turnAround ()
 stepForward()
+
+function sayGoodMorning(){
+  console.log("Good Morning ");
+}
+sayGoodMorning()
+
+/*
+if (isAwake) {
+  console.log("Awake!");
+}
+else {
+  console.log("Asleep!");
+}
+isAwake = true;
+*/
+
+function sayManyThings (){
+ console.log("I can say ine thing.");
+ return;
+ console.log("But can I say many things?");
+}
+
+function getBankBalance () {
+  return "You are cool";
+}
+/*
+console.log(getBankBalance());
+
+function addOne(inputNumber) {
+ inputNumber += 1;
+console.log(inputNumber);
+  }
+  addOne(inputNumber:8);
+*/
+
+/*
+function square (numberToSquare) {
+  return numberToSquare * numberToSquare;
+}
+let squaredNumber = square(numberToSquare:15);
+console.log(squaredNumber);
+*/
+
+function doubleNumber (numberToDouble) {
+  console.log(numberToDouble *= 2);
+}
+doubleNumber(4); // Output: 8
+
+
+
