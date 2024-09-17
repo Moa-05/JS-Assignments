65de755865c872df824eb8dd6e09c9290f9568c3
Moa-05 <moaber654@ihmeducation.se>
2024-09-17 14:51:48 +0200

    summery

---
+++  P13-HTMLDOM/js/app.js
@@ -0,0 +1,17 @@
+const headerText5 = document.getElementById("header5");
+headerText5.innerHTML = "I AM LOST";
+
+headerText5.style.color = "red";
+headerText5.style.backgroundColor = "black";
+
+const headerText6 = document.getElementById("button0");
+headerText6.style.backgroundColor = "red";
+headerText6.style.color = "white";
+
+const textField0 = document.getElementById("text0");
+textField0.innerHTML = multiplyNumber(5);
+
+function multiplyNumber (numberToMultiply) {
+  return numberToMultiply * numberToMultiply;
+}
+
