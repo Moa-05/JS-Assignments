7679f8d0b46d231d90c45db75ac6b888488cfafc
Moa-05 <moaber654@ihmeducation.se>
2024-09-19 10:47:34 +0200

    Refactor to impro

--- AnythingYouWant/js/app.js
+++ AnythingYouWant/js/app.js
@@ -1,11 +1,27 @@
+
 const textField0 = document.getElementById("text0");
-textField0.innerHTML =  showStylizedScore(786);
+const textField1 = document.getElementById("text1");
+
+let score = 0;
+
+increaseScoreByOne();
 
 function showScore (inputNumber) {
   return inputNumber * 100;
 }
-function showStylizedScore( scoreInput) {
+function showStylizedScore(scoreInput) {
   return "Your Score is: " + scoreInput * 100;
 }
 
+function increaseScoreByOne () {
+  score++;
+  updateScoreText(score);
+}
+
+function updateScoreText(newText) {
+ return textField0.innerHTML = newText;
+}
+function updateStatusText(newText) {
+ return textField1.innerHTML = newText;
+}
 
